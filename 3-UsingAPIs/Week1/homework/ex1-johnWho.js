6cbb4043d45f1dbb16425fbed809a7e260424fd3
Fikret Yilmaz <yilmz.fikret@gmail.com>
2021-08-28 16:30:54 +0200

    exer-1 solved

--- 3-UsingAPIs/Week1/homework/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/homework/ex1-johnWho.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const { reject } = require('lodash');
+
 /*------------------------------------------------------------------------------
 Rewrite this function, but replace the callback syntax with the Promise syntax:
 - Have the `getAnonName` function return a `new Promise`.
@@ -7,20 +10,27 @@ Rewrite this function, but replace the callback syntax with the Promise syntax:
   didn't pass in a first name!"
 ------------------------------------------------------------------------------*/
 // TODO see above
-const getAnonName = (firstName, callback) => {
-  setTimeout(() => {
-    if (!firstName) {
-      callback(new Error("You didn't pass in a first name!"));
-      return;
-    }
+function getAnonName(firstName) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!firstName) {
+        reject(new Error("You didn't pass in a first name!"));
+      } else {
+        const fullName = `${firstName} Doe`;
+        resolve(fullName);
+      }
+    }, 1000);
+  });
+  return promise;
+}
 
-    const fullName = `${firstName} Doe`;
-
-    callback(fullName);
-  }, 1000);
-};
-
-getAnonName('John', console.log);
+getAnonName('John', console.log)
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // ! Do not change or remove the code below
 module.exports = getAnonName;
